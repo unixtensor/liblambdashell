15a2dcb3d5aeb129cae2738114f5bcf5a76a3910
rhpidfyre <brandon@rhpidfyre.io>
2025-01-14 16:38:04 -0500

    experimenting

--- src/session.rs
+++ src/session.rs
@@ -1,10 +1,8 @@
-use std::{cell::RefCell, fs, io::{self}, rc::Rc};
+use std::{fs, io::{self}, sync::{Arc, Mutex}, thread};
 use core::fmt;
 use color_print::ceprintln;
 
-use crate::{
-	commands, history::History, ps::{self, Ps}, rc::{self}, vm::{self, LuauVm}
-};
+use crate::{commands, history::History, ps::{self, Ps}, rc::{self}, vm::{self, LuauVm}};
 
 #[inline]
 pub fn shell_error<E: fmt::Display>(err: E) {
@@ -28,6 +26,42 @@ impl<T, E: fmt::Display> MapDisplay<T, E> for Result<T, E> {
 	}
 }
 
+trait Errors {
+	fn error<E: fmt::Display>(&mut self, err: E);
+}
+impl Errors for LambdaShell {
+	fn error<E: fmt::Display>(&mut self, err: E) {
+		shell_error(err);
+		self.terminate = true;
+	}
+}
+
+trait Signals {
+	fn sigterm_event(&mut self);
+	fn input(&mut self);
+}
+impl Signals for LambdaShell {
+	fn sigterm_event(&mut self) {
+		// task::spawn(async move {
+		// 	signal::ctrl_c().await.expect("Failed to listen for a sigterm signal.");
+		// 	// self.history.write_to_file_fallible();
+		// 	std::process::exit(0x0100);
+		// });
+	}
+	fn input(&mut self) {
+		thread::spawn(|| {
+			loop {
+				if self.terminate { break } else {
+					match self.wait() {
+				        Ok(()) => self.ps.borrow().display(),
+				        Err(flush_err) => self.error(flush_err),
+				    }
+				}
+			}
+		});
+	}
+}
+
 #[derive(Debug, Clone)]
 pub struct Config {
 	pub norc: bool
@@ -37,13 +71,13 @@ pub struct LambdaShell {
 	history: History,
 	config: Config,
 	vm: LuauVm,
-	ps: Rc<RefCell<Ps>>,
+	ps: Arc<Mutex<Ps>>,
 }
 impl LambdaShell {
 	pub fn create(config: Config) -> Self {
-		let ps = Rc::new(RefCell::new(Ps::set(ps::DEFAULT_PS.to_owned())));
+		let ps = Arc::new(Mutex::new(Ps::set(ps::DEFAULT_PS.to_owned())));
 		Self {
-			ps: Rc::clone(&ps),
+			ps: Arc::clone(&ps),
 			vm: vm::LuauVm::new(ps),
 			history: History::init(),
 			terminate: false,
@@ -51,7 +85,7 @@ impl LambdaShell {
 		}
 	}
 
-	pub fn wait(&mut self) -> Result<(), io::Error> {
+	fn wait(&mut self) -> Result<(), io::Error> {
 		io::Write::flush(&mut io::stdout()).map(|()| {
 			let mut input = String::new();
 			io::stdin().read_line(&mut input).map_or_display(|_size| match input.trim() {
@@ -64,31 +98,14 @@ impl LambdaShell {
 		})
 	}
 
-	pub fn error<E: fmt::Display>(&mut self, err: E) {
-		shell_error(err);
-		self.terminate = true;
-	}
-
-	pub fn start(&mut self) {
+	pub async fn start(&mut self) {
 		if !self.config.norc {
 			if let Some(conf_file) = rc::config_file() {
-				fs::read_to_string(conf_file).map_or_display(|luau_conf| self.vm_exec(luau_conf));
+				fs::read_to_string(conf_file).map_or_display(|luau_conf| self.vm.exec(luau_conf));
 			}
-		}
+		};
 		self.ps.borrow().display();
-
-		loop {
-			if self.terminate { break } else {
-				match self.wait() {
-			        Ok(()) => self.ps.borrow().display(),
-			        Err(flush_err) => self.error(flush_err),
-			    }
-			}
-		}
-		self.history.write_to_file_fallible();
-	}
-
-	pub fn vm_exec(&self, source: String) {
-		self.vm.exec(source);
+		self.sigterm_event();
+		self.input();
 	}
 }
